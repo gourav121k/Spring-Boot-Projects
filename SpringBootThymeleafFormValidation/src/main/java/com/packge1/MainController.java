1888b5a6c5f3954e3b75fc76ead656d41e8c05e9
Gourav Kumar <gourav121k@gmail.com>
2020-08-24 08:37:53 +0530

    Thymeleaf Form Validation

---
+++ SpringBootThymeleafFormValidation/src/main/java/com/packge1/MainController.java
@@ -0,0 +1,43 @@
+package com.packge1;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/register")
+	public String showForm(Model model) {
+		User user = new User();
+		model.addAttribute("user", user);
+		
+		List<String> listProfession = Arrays.asList("Developer", "Tester", "Architect");
+		model.addAttribute("listProfession", listProfession);
+		
+		return "register_form";
+	}
+	
+	@PostMapping("/register")
+	public String submitForm(@Valid @ModelAttribute("user") User user,
+			BindingResult bindingResult, Model model) {
+		System.out.println(user);
+		
+		if (bindingResult.hasErrors()) {
+			List<String> listProfession = Arrays.asList("Developer", "Tester", "Architect");
+			model.addAttribute("listProfession", listProfession);
+			
+			return "register_form";
+		} else {
+			return "register_success";
+		}
+	}
+}
---
+++ SpringBootThymeleafFormValidation/src/main/java/com/packge1/SpringBootThymeleafFormValidationApplication.java
@@ -0,0 +1,13 @@
+package com.packge1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeleafFormValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThymeleafFormValidationApplication.class, args);
+	}
+
+}
